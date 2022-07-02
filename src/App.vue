80cf30b784fbe006eff02848b1952f20a68837cd
liguangxin <liguangxin2107@126.com>
2022-07-02 23:32:52 +0800

    first demo for vue scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <PersonDemo />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+  import PersonDemo from './components/PersonDemo.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PersonDemo
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/PersonDemo.vue
@@ -0,0 +1,38 @@
+/* 配置组件模版 */
+<template>
+    <div>
+        <h2>人员信息：</h2><br>
+        <h2 class="name">姓名: {{name}}</h2><br>
+        <h2 class="age">年龄: {{age}}</h2><br>
+        <button @click.prevent="showInfo">点击打印信息</button>
+    </div>
+</template>
+
+/* 配置组件数据、交互、事件等等 */
+<script>
+    // 此处只暴露组件配置，并没有创建组件
+    export default {
+        data(){
+            return {
+                name: 'Tom',
+                age: 18
+            }
+        },
+        methods: {
+            showInfo(){
+                // this指向组件实例， VueComponent实例
+                console.log(`name: ${this.name}, age: ${this.age}`);
+            }
+        }
+    }
+</script>
+
+/* 配置组件样式 */
+<style>
+    .name {
+        background-color: blue;
+    }
+    .age {
+        background-color: orange;
+    }
+</style>
