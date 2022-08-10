1986cfd26d7f72666d79fed2e5e114b54bacd2d7
liguangxin <liguangxin2107@126.com>
2022-08-10 21:49:46 +0800

    modify files

--- src/App.vue
+++ src/App.vue
@@ -30,6 +30,13 @@
 </template>
 
 <script>
+    /**
+     * Header： 非路由组件
+     * 路由组件和非路由组件的却别：
+     * 1. 注册：非路由组件是注册在要使用的组件当中，而路由组件是注册在路由配置当中的
+     * 2. 使用：非路由组件使用注册的组件标签， 而路由组件使用声明式导航(router-link, router-view)和编程式导航来使用(push/replace)
+     * 3. 生命周期： 路由组件在路径切换时，会销毁重建(keep-alive可阻止此操作), 而非路由组件不会
+     */
     import Header from './components/Header'
     export default {
         name: 'App',
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false
+  lintOnSave:false //项目默认安装了eslint语法检查工具，而且默认检查很严格，开发阶段禁用
 })
